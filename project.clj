388afcf5963c114ca48250d2320aeca998c28bcd
Frsf Nrrg <frsfnrrg@gmail.com>
2013-06-16 11:42:25 -0400

    Whatever

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :java-source-paths ["src/gipfj"]
+  :aot [gipf.core]
   :main gipf.core)
